df4eb7bd649dc43821aef8d728cf8a910cb73a43
Rohit <rohit.karma026@gmail.com>
2024-12-25 14:47:17 +0530

    profiles setup

--- e-commerce/src/main/java/com/r_tech/ecommerce/configuration/SwaggerConfig.java
+++ e-commerce/src/main/java/com/r_tech/ecommerce/configuration/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.r_tech.ecommerce.configuration;
 
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
